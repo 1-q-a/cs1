121c148c8b3b0751d0fc5432762cd10f3d8c4ac0
s1f10230306 <s1f102303068@iniad.org>
2023-07-01 19:54:36 +0900

    d

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 import re
 from flask import Flask, render_template, request
+from markupsafe import Markup  # 追加
+
 app = Flask(__name__)
 
 kinoko_count = 3
 takenoko_count = 5
 messages = ['Kinoko is wonrderful!', 'Takenoko is awesome!']
+
+# URLをリンクに変換する関数を定義
+def convert_urls_to_links(text):
+    # 正規表現パターンを定義
+    url_pattern = r'(https?://\S+)'
+
+    def replace_url(match):
+        url = match.group(1)
+        return '<a href="{}">{}</a>'.format(url, url)
+
+    return re.sub(url_pattern, replace_url, text)
 
 @app.route('/')
 def top():
@@ -33,8 +46,9 @@ def answer():
         message = re.sub(r'>', r'&gt;', message)
         message = re.sub(r'\*(.+)\*', r'<strong>\1</strong>', message)
         message = re.sub(r'(\d{2,3})-\d+-\d+', r'\1-****-****', message)
+        message = convert_urls_to_links(message)  # URLをリンクに変換
         message_html += '<div class="alert {1}" role="alert">{0}</div>\n'.format(
-            message, 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5')
+            Markup(message), 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5')  # Markupでエスケープを無効化
 
     return render_template('vote.html', **vars())
 
